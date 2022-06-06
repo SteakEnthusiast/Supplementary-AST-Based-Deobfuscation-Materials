f47ab82b79087ccd5382480e1a600354911bbca6
SteakEnthusiast <Williamkhemmarquez@gmail.com>
2022-06-06 00:40:37 -0400

    More examples in unreachableLogicalCodeObfuscated.js

--- Dead Code Removal/If Statement and Logical Expression Simplification/unreachableLogicalCodeObfuscated.js
+++ Dead Code Removal/If Statement and Logical Expression Simplification/unreachableLogicalCodeObfuscated.js
@@ -1,16 +1,24 @@
 if(!![]){
 	console.log("This always runs! 1")
+}else{
+    console.log("This never runs.")
 }
 
 if (40 > 80){
 	console.log("This never runs.")
+}else if (1 < 2){
+    console.log("This always runs! 2")
+}else{
+    console.log("This never runs.")
 }
 
-![] ? console.log("This never runs.") : console.log("This always runs! 2") 
+![] ? console.log("This never runs.") : console.log("This always runs! 3") 
 
 // Chained example
 
 if (!![]){
-	console.log("This always runs! 3")
-  	![] ? console.log("This never runs.") : (false) ? console.log("This never runs") : (40 < 20) ? console.log("This never runs.") : 80 > 1 ? console.log("This always runs! 4") : 40 > 2 ? console.log("This never runs.")  : console.log("This never runs.")
+	console.log("This always runs! 4")
+  	![] ? console.log("This never runs.") : (false) ? console.log("This never runs") : (40 < 20) ? console.log("This never runs.") : 80 > 1 ? console.log("This always runs! 5") : 40 > 2 ? console.log("This never runs.")  : console.log("This never runs.")
+}else{
+    console.log("This never runs.")
 }
