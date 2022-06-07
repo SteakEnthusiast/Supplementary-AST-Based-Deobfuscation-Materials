3b3e147fb483bcae869293e00104c84b67677f4c
SteakEnthusiast <Williamkhemmarquez@gmail.com>
2022-06-07 04:28:03 -0400

    Added documentation

--- Proxy Functions/Binary Expression & Unary Expression Proxy Functions/AST-Based Simplfication/deobfuscator.js
+++ Proxy Functions/Binary Expression & Unary Expression Proxy Functions/AST-Based Simplfication/deobfuscator.js
@@ -24,24 +24,41 @@ function deobfuscate(source) {
     FunctionDeclaration(path) {
       const { node, scope } = path;
       const { id, body, params } = node;
+      // Check that function has a one-line body, and returns immediately.
       if (!t.isReturnStatement(body.body[0])) return;
       const proxyExpression = body.body[0].argument;
+      // Handle only BinaryExpressions or UnaryExpressions.
       if (
         !t.isBinaryExpression(proxyExpression) &&
         !t.isUnaryExpression(proxyExpression)
       )
         return;
 
+      // This will become false if the function is referenced, but not within a CallExpression. In that case, we can't remove the original function, but we can still simplify all calls to it
+      let shouldDelete = true;
+
       const { constant, referencePaths } = scope.getBinding(id.name);
+      // If function is redefined somewhere, don't
       if (!constant) return;
+
+      // Iterate over all times the proxy function is referenced.
+
       for (let referencePath of referencePaths) {
+        // referencePath will be a path to an Identifier. To check if it's contained in a CallExpression, we should check it's parentPath.
         let { parentPath } = referencePath;
 
-        if (!t.isCallExpression(parentPath)) return;
+        // Don't delete if referenced, but not in a call expression.
+        // This is implemented for sake of generality. Perhaps your obfuscated file has a self-integrity check somewhere idk lol
+        if (!t.isCallExpression(parentPath)) {
+          shouldDelete = false;
+        }
 
+        // Clone the expression into it's own AST so we can modify it
         const proxyExpressionCopyAst = parser.parse(
           generate(proxyExpression).code
         );
+
+        // This visitor replaces the variables in the proxy expression with the actual arguments. Correct ordering of the arguments is maintained.
         const replaceVarsInExpressionWithArguments = {
           Identifier(_path) {
             for (let i = 0; i < params.length; i++) {
@@ -52,10 +69,16 @@ function deobfuscate(source) {
             }
           },
         };
+
+        // Execute the visitor
         traverse(proxyExpressionCopyAst, replaceVarsInExpressionWithArguments);
+
+        // Replace the CallExpression with the modified proxy expression
         parentPath.replaceWithMultiple(proxyExpressionCopyAst.program.body);
       }
-      path.remove();
+      if (shouldDelete) {
+        path.remove();
+      }
     },
   };
 
